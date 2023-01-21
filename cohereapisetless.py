eb5415d8267c21acc0c2364494bf9eca1ac91a19
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2023-01-21 00:57:29 -0500

    Add a setless usage of coherapi which is less accurate but has updated data set

---
+++ cohereapisetless.py
@@ -0,0 +1,14 @@
+import cohere
+import keys
+def grabDefinition(word: str):
+    co = cohere.Client(keys.cohereapikey)
+    response = co.generate(
+    model='command-xlarge-nightly',
+    prompt=f'What is {word}?',
+    max_tokens=50,
+    temperature=0.8,
+    stop_sequences=["--"],
+  )
+    return (response.generations[0].text)
+
+print(grabDefinition('Wood'))
